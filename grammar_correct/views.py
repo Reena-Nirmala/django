99d2e1239be247a339973e4a00370d51e4f25748
Savireddy Reena <84865491+Reena-Nirmala@users.noreply.github.com>
2024-02-12 14:47:23 +0530

    Update views.py

--- grammar_correct/views.py
+++ grammar_correct/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from language_tool_python import LanguageTool
 from django.http import JsonResponse
+from Text_app.grammar_correct.models import GrammarCheck
 
 def grammar_checker(request):
     if request.method == 'POST':
@@ -19,7 +20,11 @@ def grammar_checker(request):
             'corrections': [{'ruleId': match.ruleId, 'message': match.message, 'replacements': match.replacements} for match in matches],
             'corrected_text': corrected_text,
         }
+        user_id = request.session.get('user_id')
 
+        gram_check = GrammarCheck(user1=user_id, input_text=input_text, corrected_text=corrected_text)
+        gram_check.save()
+        
         # return JsonResponse(response_data)
         return render(request,'grammar.html',{'result':corrected_text,'input':input_text})
 
